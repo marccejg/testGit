1651b146e71e295850d650edb4b814d3ebd9970c
marccejg <marcejgarcia@gmail.com>
2024-12-15 20:29:13 -0300

    sexto commit

--- git.ts
+++ git.ts
@@ -1,4 +1,5 @@
 console.log("primer commit");
 console.log("segundo commit");
 console.log("tercer commit");
-console.log("cuerto commit")
+console.log("cuerto commit");
+console.log("sexto comit");
